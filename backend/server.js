ed93dd43ffab984b4eb631c4c8fe9a425fd29224
Luke Olali <luke.olali@andela.com>
2018-10-01 21:30:28 +0300

    Adds middleware to check if authorization header has Bearer scheme and verifies its validity

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ server.use(jsonServer.defaults());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json())
 
-const createTokem = (payload) => jwt.sign(package, 'SECRET_KEY', { expiresIn: '1h' });
+const createTokem = (payload) => jwt.sign(payload, 'SECRET_KEY', { expiresIn: '1h' });
 const verifyToken = (token) => jwt.verify(token, 'SECRET_KEY');
 
 const userdb = JSON.parse(fs.readFileSync('./database.json', 'UTF-8')).users || [];
@@ -29,4 +29,27 @@ server.post('/auth/login', (req, res) => {
   }
   const access_token = createTokem({ email, passowrd });
   res.status(200).json({ access_token });
+});
+
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
+  if (req.method === 'GET') {
+    next();
+  } else {
+    if (req.headers.authorization === undefined || 
+        req.headers.authorization.split(' ')[0] !== 'Bearer') {
+      const status = 401;
+      const message = 'Bad authorization header';
+      res.status(status).json({ status, message });
+      return;
+    }
+
+    try {
+      verifyToken(req.headers.authorization.split(' ')[1])
+      next();
+    } catch (err) {
+      const status = 401;
+      const message = 'Error: access token is not valid';
+      res.status(status).json({ status, message });
+    }
+  }
 })
