e54de03129b7bbd345b3ad27af9605bd9ea5e548
Luke Olali <luke.olali@andela.com>
2018-10-02 20:54:30 +0300

    Adds component declarations to the main module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ContactUpdateComponent } from './contact-update/contact-update.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactCreateComponent,
+    ContactUpdateComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule
