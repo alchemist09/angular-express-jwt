7ff496435ec979c2b8bc103c6b7b6ca72e12be44
Luke Olali <luke.olali@andela.com>
2018-10-22 17:18:02 +0300

    Imports the Http module to the main module and updates the modules array

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { routingModule } from './routing.module';
 
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { ContactDetailComponent } from './contact-detail/contact-detail.componen
   ],
   imports: [
     BrowserModule,
-    routingModule
+    routingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
