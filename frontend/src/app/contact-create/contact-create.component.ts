bbce35ed28ee53e762d5efa40682cc9b11370dbb
Luke Olali <luke.olali@andela.com>
2018-10-23 18:01:23 +0300

    Adds logic for creating form used in creating contacts

--- frontend/src/app/contact-create/contact-create.component.ts
+++ frontend/src/app/contact-create/contact-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-create',
@@ -10,6 +11,31 @@ export class ContactCreateComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public contactForm = new FormGroup({
+    firstName: new FormControl("", [Validators.required]),
+    lastName: new FormControl("", [Validators.required]),
+    email: new FormControl("",
+      [Validators.required,
+       Validators.pattern('[a-zA-z0-9_\.]+@[a-zA-Z]+\.[a-zA-Z]+')]
+    ),
+    phone: new FormControl("", [Validators.pattern('[0-9]+')]),
+    city: new FormControl(),
+    country: new FormControl(),
+    title: new FormControl()
+  });
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get phone() {
+    return this.contactForm.get('phone');
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
   }
 
 }
