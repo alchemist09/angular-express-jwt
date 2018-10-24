c6d12928421d3485b33dade2a0aaa571dd969fe3
Luke Olali <luke.olali@andela.com>
2018-10-24 22:17:08 +0300

    Creates logic for accessing data passed from openDialog() method in the dialog component

--- frontend/src/app/contact-detail/contact-detail.component.ts
+++ frontend/src/app/contact-detail/contact-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { Contact } from '../contact';
 
 @Component({
   selector: 'app-contact-detail',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Contact) { }
 
   ngOnInit() {
   }
