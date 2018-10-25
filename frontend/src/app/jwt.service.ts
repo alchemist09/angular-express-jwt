2e95f611072df615a54fd7153adb42541505e2b1
Luke Olali <luke.olali@andela.com>
2018-10-25 18:21:46 +0300

    Creates functionality for logout, login and checking if a user is logged in

--- frontend/src/app/jwt.service.ts
+++ frontend/src/app/jwt.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  login(email: string, password: string) {
+    return this.httpClient.post<{access_token: string}>
+    ('/auth/login', { email, password })
+    .pipe(tap(res => {
+      localStorage.setItem('access_token', res.access_token);
+    }))
+  }
+
+  logout() {
+    localStorage.removeItem('access_token');
+  }
+
+  loggedIn(): boolean {
+    return localStorage.getItem('access_token') != null
+  }
 }
